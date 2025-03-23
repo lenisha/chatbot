9bd22324d839c60784e901bae3686dc7a5bde148
Elena Neroslavskaya <lenisha@gmail.com>
2025-03-23 01:59:50 -0400

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,7 @@
 import streamlit as st
 from openai import OpenAI
+from openai import AzureOpenAI
+    
 
 # Show title and description.
 st.title("💬 Chatbot")
@@ -13,12 +15,19 @@ st.write(
 # Alternatively, you can store the API key in `./.streamlit/secrets.toml` and access it
 # via `st.secrets`, see https://docs.streamlit.io/develop/concepts/connections/secrets-management
 openai_api_key = st.text_input("OpenAI API Key", type="password")
+openai_api_endpoint = st.text_input("OpenAI Endpoint")
+
 if not openai_api_key:
     st.info("Please add your OpenAI API key to continue.", icon="🗝️")
 else:
 
     # Create an OpenAI client.
-    client = OpenAI(api_key=openai_api_key)
+    #client = OpenAI(api_key=openai_api_key)
+    client = AzureOpenAI(
+        api_key=openai_api_key,  
+        api_version="2024-02-15-preview",
+        azure_endpoint = openai_api_endpoint
+    )
 
     # Create a session state variable to store the chat messages. This ensures that the
     # messages persist across reruns.
@@ -41,7 +50,7 @@ else:
 
         # Generate a response using the OpenAI API.
         stream = client.chat.completions.create(
-            model="gpt-3.5-turbo",
+            model="gpt-35-turbo-16k",
             messages=[
                 {"role": m["role"], "content": m["content"]}
                 for m in st.session_state.messages
